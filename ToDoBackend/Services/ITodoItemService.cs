20f2000211108fffc2a8071d3faeeebc04107463
milchr <milchr96@gmail.com>
2024-01-14 14:38:25 +0100

    Add TodoItemService

---
+++ ToDoBackend/Services/ITodoItemService.cs
@@ -0,0 +1,13 @@
+﻿using ToDoBackend.Dtos;
+
+namespace ToDoBackend.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItemDto> GetTodoItem(int id);
+        Task<List<TodoItemDto>> getAllTodoItems();
+        Task<TodoItemDto> createTodoItem(TodoItemDto todoItemDto);
+        Task<TodoItemDto> updateTodoItem(TodoItemDto todoItemDto);
+        void deleteTodoItem(int id);
+    }
+}
---
+++ ToDoBackend/Services/TodoItemService.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ToDoBackend.data;
+using ToDoBackend.Dtos;
+
+namespace ToDoBackend.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        private readonly ToDoContext db;
+        private readonly IMapper mapper;
+
+        public TodoItemService(ToDoContext db, IMapper mapper)
+        {
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        public Task<TodoItemDto> createTodoItem(TodoItemDto todoItemDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void deleteTodoItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<TodoItemDto>> getAllTodoItems()
+        {
+            var items = await db.TodoItems.ToListAsync();
+            return mapper.Map<List<TodoItemDto>>(items);
+        }
+
+        public async Task<TodoItemDto> GetTodoItem(int id)
+        {
+            var task = await db.TodoItems.FindAsync(id);
+            return mapper.Map<TodoItemDto>(task);
+        }
+
+        public Task<TodoItemDto> updateTodoItem(TodoItemDto todoItemDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
