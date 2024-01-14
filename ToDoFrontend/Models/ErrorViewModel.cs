de0b662325d040d84d5fcc89943cfe6a2498cd46
milchr <milchr96@gmail.com>
2024-01-14 14:12:25 +0100

    Add project files.

---
+++ ToDoFrontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ToDoFrontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
